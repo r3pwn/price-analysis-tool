0dbe8cbb2e6d10782d1ce4748f54147ed079f4f8
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-09-11 23:23:15 -0400

    feat(products): add product querying API

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -1,5 +1,6 @@
 import { CollectionConfig } from 'payload/types';
 import { PRODUCT_ID_TYPES, VENDORS } from '../constants';
+import { Endpoint } from 'payload/config';
 
 const Product: CollectionConfig = {
   slug: 'products',
@@ -51,6 +52,45 @@ const Product: CollectionConfig = {
         }
       ]
     }
+  ],
+  endpoints: [
+    // automatically create an API for each ID type
+    ...(PRODUCT_ID_TYPES.map((idType) => ({
+      path: `/by${idType.toLowerCase()}/:itemId`,
+      method: "get",
+      handler: async (req, res) => {
+        const { itemId } = req.params;
+
+        if (!itemId) {
+          res.status(400).send({ error: "no product id provided" });
+          return;
+        }
+        
+        const products = await req.payload.find({
+          collection: 'products',
+          where: {
+            and: [
+              {
+                'productIds.idType': {
+                  equals: idType
+                }
+              },
+              {
+                'productIds.idValue': {
+                  equals: itemId
+                }
+              }
+            ]
+          }
+        });
+
+        if (products.docs?.length) {
+          res.status(200).send(products.docs);
+        } else {
+          res.status(204).send({ error: "no product found" });
+        }
+      }
+    })) as Omit<Endpoint, "root">[])
   ]
 }
 
